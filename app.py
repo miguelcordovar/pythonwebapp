38970f92267777bab1c1ce9c6ebbd33944f7006f
Miguel Cordova <m.cordova@tcs.com>
2023-07-03 22:57:24 -0500

    Add Web files

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World Welcome Flask!"
+@app.get("/")
+def index_get():
+  return render_template("base.html")
 
 if __name__ == '__main__':
     app.run()
